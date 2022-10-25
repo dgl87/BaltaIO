30eda5897c2a85adf82699c593054033a562ad5a
Douglas Paiva <dpaiva@direct.one>
2022-10-24 23:19:54 -0300

    Stopwatch first commit

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 60 segundos");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+        }
+    }
+}
